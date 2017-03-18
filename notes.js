091b2560f62acebbfe26a64b7b7466c8203b74c7
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-17 21:23:39 -0400

    implements the function for adding notes in json format

--- notes.js
+++ notes.js
@@ -1,7 +1,25 @@
 console.log('starting notes.js');
-
+const fs = require('fs');
 var addNote = (title, body) =>{
-console.log('adding note', title,body);
+var notes = [];
+var note = {
+    title,
+    body
+};
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+   
+    } catch (e){
+
+    }
+   
+   var duplicateNotes = notes.filter((note)=>note.title === title);
+
+    if (duplicateNotes.length === 0){
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 var getAll = ()=>{
 console.log('getting all notes');
