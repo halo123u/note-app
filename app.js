60ff94be583816c893bdd130cab912429c3dbd65
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-18 19:22:38 -0400

    Added description of commands and help using yargs

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const argv = yargs.argv;
+const titleOptions = {
+    describe: 'Title of the note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Content of the Note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command( 'add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Retrieve a Note',{
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 
 if(command === 'add'){
